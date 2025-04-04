54cbb2f97db3a4a6dd4be0da952f34d2de15992f
Tidostoken <97735306+Tidostoken@users.noreply.github.com>
2025-04-04 16:45:23 -0700

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}",
   ],
   presets: [require("@relume_io/relume-tailwind")],
   theme: {
