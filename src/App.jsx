b57d46ed2f9f1322bddc3d691a585b486161b7c1
Tidostoken <97735306+Tidostoken@users.noreply.github.com>
2025-04-04 17:38:33 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import Page from "./index"; // چون فایل index.jsx در src هست
-import "./index.css";
+import { Navbar11 } from "./components/Navbar11";
+import { Header44 } from "./components/Header44";
+import { Header1 } from "./components/Header1";
+import { Layout3 } from "./components/Layout3";
+import { Layout241 } from "./components/Layout241";
+import { Layout245 } from "./components/Layout245";
+import { Contact13 } from "./components/Contact13";
+import { Footer8 } from "./components/Footer8";
 
 function App() {
-  return <Page />;
+  return (
+    <div>
+      <Navbar11 />
+      <Header44 />
+      <Header1 />
+      <Layout3 />
+      <Layout241 />
+      <Layout245 />
+      <Contact13 />
+      <Footer8 />
+    </div>
+  );
 }
 
 export default App;
